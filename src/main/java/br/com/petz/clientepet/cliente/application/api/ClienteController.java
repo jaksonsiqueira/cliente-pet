e8551145f6900d90e7d7d9d763ae003e577dc042
jaksonpsiqueira <jaksonpsiqueira@gmail.com>
2022-07-26 15:07:37 -0300

    implenta clienteApplication e novo construtor cliente

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -2,7 +2,7 @@ package br.com.petz.clientepet.cliente.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.petz.clientepet.cliente.application.api.service.ClienteService;
+import br.com.petz.clientepet.cliente.application.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
