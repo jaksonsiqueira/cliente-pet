0edbb4758c95a679f8398f66eae8446f7de14222
jaksonpsiqueira <jaksonpsiqueira@gmail.com>
2022-07-26 21:43:26 -0300

    implenta cliente repository response controller teste ok!

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteListResponse.java
@@ -0,0 +1,21 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import java.util.List;
+import java.util.UUID;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+
+public class ClienteListResponse {
+	private UUID idCliente;
+	private String nomeCompleto;	
+	private String email;	
+	private String celular;
+	private String cpf;
+	
+	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
+		return null;
+	}
+	
+	
+
+}
